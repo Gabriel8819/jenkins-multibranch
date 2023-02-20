b6c1de390cb4da4fddaa4b67cd808cb96aa35070
gabriel boyer-Beaulieu <gabriel8819@hotmail.com>
2023-02-20 14:23:45 -0500

    last commit

--- src/main/java/multibranch/Printer.java
+++ src/main/java/multibranch/Printer.java
@@ -4,7 +4,9 @@ public class Printer {
 
 	public String print(String s) {
 		System.out.println("Hello world 2");
-		
+
+		System.out.println("one");
+		System.out.println("two");
 		
 		return "Printer----ert " + s;
 	}
