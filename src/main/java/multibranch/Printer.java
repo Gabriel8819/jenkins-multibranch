09a36a038448492af8dbc23b8dc0012d4dd7e929
gabriel boyer-Beaulieu <gabriel8819@hotmail.com>
2023-02-25 22:35:42 -0500

    add before remote commit

---
+++ src/main/java/multibranch/Printer.java
@@ -0,0 +1,16 @@
+package multibranch;
+
+public class Printer {
+
+	public String print(String s) {
+
+		System.out.println("Hello world 2");
+		
+		//test
+		
+
+
+		return "Printer----------ert " + s;
+	}
+	
+}
